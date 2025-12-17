a321dd2b8b0b8fe80dc059a1d26991c1504e52ef
sj1897 <sahil.jamwal.1999@gmail.com>
2025-12-17 13:46:10 +0530

    Updates

---
+++ precache-manifest.ee70a0ad2c1f8fa61d68a2080f999e22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942882bcb084fe923f80f445e289291b",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "7b0d04ae5af198f5d1ad",
+    "url": "/shoppingcart/static/css/main.d036b651.chunk.css"
+  },
+  {
+    "revision": "34215cd1a2c8b84a688a",
+    "url": "/shoppingcart/static/js/2.4e5054d8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shoppingcart/static/js/2.4e5054d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0d04ae5af198f5d1ad",
+    "url": "/shoppingcart/static/js/main.71219693.chunk.js"
+  },
+  {
+    "revision": "c8a3c4709c24fd9daae5",
+    "url": "/shoppingcart/static/js/runtime-main.52708e56.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/shoppingcart/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/shoppingcart/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/shoppingcart/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
